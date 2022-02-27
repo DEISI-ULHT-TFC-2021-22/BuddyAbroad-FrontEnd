a7cdf1c408efce034a80c4697aa6a52494ee20b4
TomasNeto-a21903361 <a21903361@alunos.ulht.pt>
2022-02-27 15:25:39 +0000

    upload file button working in create profile, need the other 3 fields: description, languages, hobbies

--- src/app/new-profile-creation/new-profile-creation.page.ts
+++ src/app/new-profile-creation/new-profile-creation.page.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router} from '@angular/router';
 import {NavController} from '@ionic/angular';
@@ -8,14 +9,61 @@ import {NavController} from '@ionic/angular';
   styleUrls: ['./new-profile-creation.page.scss'],
 })
 export class NewProfileCreationPage implements OnInit {
+  private file: File;
+  user: any = []
 
-  constructor(private router: Router, private navCtrl: NavController) { }
+  constructor(private router: Router, private navCtrl: NavController, private http: HttpClient) { }
 
   ngOnInit() {
+    this.sync();
   }
 
   goback() {
     this.navCtrl.pop();
   }
+
+  onFileChange(fileChangeEvent) {
+    this.file = fileChangeEvent.target.files[0];
+  }
+
+  async submitForm() {
+    let formData = new FormData();
+    formData.append("image: ", this.file, this.file.name)
+    
+    let postData = {
+      "id": 73,
+      "interests": [
+        {
+            "name": ""
+        }
+      ],
+      "languages": [
+          {
+              "name": ""
+          }
+      ],
+      "name": this.user.name,
+      "email": this.user.email,
+      "dob": this.user.dob,
+      "phone": this.user.phone,
+      "image": this.file.name,
+      "description": "",
+      "rating": 5,
+      "tourcount": 8,
+      "guide": null
+    }; 
+
+    this.http.put("http://18.171.19.26/users/73", postData)
+    .subscribe((response) => console.log(response))
+  }
+
+
+sync() {
+  this.http.get('http://18.171.19.26/users/72')
+  .subscribe(data => {
+    console.log(data)
+    this.user = data;
+  })
+}
 
 }
